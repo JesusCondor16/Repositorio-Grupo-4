a637e1f52f66f8b1ea4ee7ac82f10eb01affc7b0
Daniel Tintaya <daniel.tintaya@unmsm.edu.pe>
2023-07-10 13:58:26 -0500

    Agregar interfaz login

--- src/App.java
+++ src/App.java
@@ -1,19 +1,21 @@
 
 //import controlador.ControladorLogin;
+import controlador.ControladorLogin;
 import modelo.Empleado;
 import modelo.EmpleadoArreglo;
+import vista.frmIngreso;
 //import vista.frmIngreso;
 public class App {
 
     public static void main(String[] args) {
         
-        //frmIngreso fIngreso = new frmIngreso();
+        frmIngreso fIngreso = new frmIngreso();
         configuracion.Datos.usuarios.agregar(new Empleado("Juan Perez", "0000001", "jperez", "abc123"));
         configuracion.Datos.usuarios.agregar(new Empleado("Jose Diaz", "0000002", "jdiaz", "abc123"));
         configuracion.Datos.usuarios.agregar(new Empleado("Carlos Ramos", "0000003", "cramos", "abc123"));
         
-        /*ControladorLogin controlador = new ControladorLogin(configuracion.Datos.usuarios, fIngreso);
-        controlador.iniciar();*/
+        ControladorLogin controlador = new ControladorLogin(configuracion.Datos.usuarios, fIngreso);
+        controlador.iniciar();
         
     }
     
